47fcdc13842e3317dfb2fa987cfa75908a61ed2a
Suresh Jangale <sureshjangale137@gmail.com>
2021-05-24 19:03:10 +0530

    Adding sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+
+
 function App() {
   return (
     // BEM naming convention
     <div className="App">
       <Header/>
-      {/* {Header} */}
-      {/* side bar */}
+      <div className="app_body">
+        <Sidebar/>
       {/* React-router -> chat screen */}
 
+      </div>
     </div>
   );
 }
