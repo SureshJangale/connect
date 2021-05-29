02221ec01e419abdfcde4098e7a4542fad1577e7
Suresh Jangale <sureshjangale137@gmail.com>
2021-05-29 19:46:11 +0530

    Adding firebase hosting

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -3,31 +3,31 @@ import { Button } from '@material-ui/core';
 
 import './LogIn.css';
 
-import {auth, provider} from '../../firebase'
+import { auth, provider } from '../../firebase'
 import { useStateValue } from '../../StateProvider';
-import {actionTypes} from '../../reducer';
+import { actionTypes } from '../../reducer';
 
 function LogIn() {
     const [state, dispatch] = useStateValue();
-    
+
     const signIn = () => {
         auth.signInWithPopup(provider)
-        .then(result => {
-            console.log(result);
-            dispatch({
-                type:actionTypes.SET_USER,
-                user:result.user,
+            .then(result => {
+                console.log(result);
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                })
             })
-        })
-        .catch(error => {
-            alert(error.message);
-        })
-    } 
+            .catch(error => {
+                alert(error.message);
+            })
+    }
 
     return (
         <div className="login">
             <div className="login_container">
-                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fslack.com%2Fmedia-kit&psig=AOvVaw2b7HZzvJXlByrYZXir2Lwr&ust=1622215733714000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICcvdqW6vACFQAAAAAdAAAAABAJ" alt=""/>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fslack.com%2Fmedia-kit&psig=AOvVaw2b7HZzvJXlByrYZXir2Lwr&ust=1622215733714000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICcvdqW6vACFQAAAAAdAAAAABAJ" alt="" />
                 <h1>
                     Sign in to Connect+
                 </h1>
